b71d9c00259ba1f98edf6482d1893aacda907d1c
Musab Khawaja <khawaja@arrival.com>
2020-05-10 00:33:26 +0100

    Add classes

---
+++ src/autotrader_scraper/advert.py
@@ -0,0 +1,90 @@
+import json
+import re
+from typing import Text, Any, Dict
+
+import requests
+
+URL = Text
+
+
+class Advert:
+    def __init__(self, url: URL):
+        self.url = url
+        self._contents = None
+
+    @property
+    def contents(self):
+        self._contents = self._contents if self._contents else self.scrape()
+        return self._contents
+
+    def scrape(self) -> Dict[Text, Any]:
+        url_id = re.findall('[0-9]{11,}', self.url)[0]
+        base_url = 'https://www.autotrader.co.uk/json/fpa/initial/'
+        response = requests.get(f"{base_url}{url_id}", timeout=5)
+
+        ret = dict()
+
+        ret['url'] = self.url
+        ret['status_code'] = response.status_code
+        if response.status_code != 200:
+            return ret
+
+        ret['raw_response'] = response.content.decode('utf-8')
+
+        d = json.loads(response.content.decode('utf-8'))
+
+        keys_vehicle = {
+            'make', 'model', 'trim', 'condition', 'tax', 'co2Emissions'
+        }
+        for nm in set(d['vehicle'].keys()).intersection(keys_vehicle):
+            ret[nm] = d['vehicle'][nm]
+
+        keys_spec = {
+            'engine-size', 'manufactured-year', 'body-type', 'mileage',
+            'transmission', 'fuel-type', 'doors', 'seats'
+        }
+        for nm in set(d['vehicle']['keyFacts'].keys()).intersection(
+                keys_spec):
+            ret[nm] = d['vehicle']['keyFacts'][nm]
+
+        if 'doors' in ret.keys():
+            match = re.search(r'\d+', ret['doors'])
+            if match:
+                ret['doors'] = match[0]
+
+        if 'seats' in ret.keys():
+            match = re.search(r'\d+', ret['seats'])
+            if match:
+                ret['seats'] = match[0]
+
+        if 'manufactured-year' in ret.keys():
+            match = re.search(r'\d{4}', ret['manufactured-year'])
+            if match:
+                ret['manufactured-year'] = match[0]
+
+        if 'mileage' in ret.keys():
+            ret['mileage'] = re.sub(r'[^\d\.]', '', ret['mileage'])
+
+        if 'co2Emissions' in ret.keys():
+            ret['co2Emissions'] = re.sub(r'[^\d\.]', '',
+                                         ret['co2Emissions'])
+
+        keys_advert = {'price', 'description'}
+        for nm in set(d['advert'].keys()).intersection(keys_advert):
+            ret[nm] = d['advert'][nm]
+
+        if 'price' in ret.keys():
+            ret['price'] = re.sub(r'[^\d]', '', ret['price'])
+
+        keys_seller = {'isTradeSeller', 'townAndDistance', 'emailAddress'}
+        for nm in set(d['seller'].keys()).intersection(keys_seller):
+            ret[nm] = d['seller'][nm]
+
+        for nm in set(d['pageData']['tracking'].keys()).intersection(
+                keys_seller):
+            ret[nm] = d['pageData']['tracking'][nm]
+
+        for nm in list(ret.keys()):
+            ret[re.sub('-', '_', nm)] = ret.pop(nm)
+
+        return ret
---
+++ src/autotrader_scraper/search_results.py
@@ -0,0 +1,46 @@
+from typing import Text, List
+
+import requests
+from lxml import html
+
+URL = Text
+
+
+class SearchResults:
+    def __init__(self, base_url: Text,
+                 max_results: int = 1000,
+                 max_pages: int = 200):
+        self.base_url = base_url
+        self.max_results = max_results
+        self.max_pages = max_pages
+        self._results = None
+
+    @property
+    def results(self) -> List[URL]:
+        if self._results:
+            return self._results
+
+        results = []
+        page_count = 1
+        while True:
+            url = f"{self.base_url}&page={page_count}"
+            urls_from_page = self.scrape(url)
+            results += urls_from_page
+
+            if len(results) > self.max_results or page_count > self.max_pages:
+                break
+            elif len(urls_from_page) == 0:
+                break
+            else:
+                page_count += 1
+
+        self._results = results[:self.max_results]
+        return self._results
+
+    @staticmethod
+    def scrape(url: Text) -> List[URL]:
+        response = requests.get(url, timeout=5)
+        tree = html.fromstring(response.content.decode('utf-8'))
+        res = set(
+            tree.xpath('//a[contains(@class, "listing-fpa-link")]/@href'))
+        return list(res)
